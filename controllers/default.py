4730ab1e7b2b86661771491c61ba6b02b98b8bfd
espern <loic.espern@gmail.com>
2013-09-20 15:21:27 +0200

    Sentiment analysis...

--- controllers/default.py
+++ controllers/default.py
@@ -1,76 +1,38 @@
 # -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
-
-#########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - call exposes all registered services (none by default)
-#########################################################################
-
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
+    form = FORM(
+            TEXTAREA(_name='sentence', requires=IS_NOT_EMPTY()),
+            INPUT(_type='submit')
+        ).process()
+    if form.accepted:
+        redirect(URL('sentence_analyser',vars=dict(sentence=form.vars.sentence)))
+    return dict(form=form)
 
-    if you need a simple wiki simple replace the two lines below with:
-    return auth.wiki()
-    """
-    response.flash = T("Welcome to web2py!")
-    return dict(message=T('Hello World'))
+def sentence_analyser():
+    import requests
+    import json
 
+    text = request.vars.sentence
+    text = text.split('_')
+    text = ' '.join(text)
 
-def user():
-    """
-    exposes:
-    http://..../[app]/default/user/login
-    http://..../[app]/default/user/logout
-    http://..../[app]/default/user/register
-    http://..../[app]/default/user/profile
-    http://..../[app]/default/user/retrieve_password
-    http://..../[app]/default/user/change_password
-    http://..../[app]/default/user/manage_users (requires membership in
-    use @auth.requires_login()
-        @auth.requires_membership('group name')
-        @auth.requires_permission('read','table name',record_id)
-    to decorate functions that need access control
-    """
-    return dict(form=auth())
+    url = 'http://text-processing.com/api/sentiment/'
+    data = {'text': text}
 
-@cache.action()
-def download():
-    """
-    allows downloading of uploaded files
-    http://..../[app]/default/download/[filename]
-    """
-    return response.download(request, db)
+    r = requests.post(url, data=data)
 
-
-def call():
-    """
-    exposes services. for example:
-    http://..../[app]/default/call/jsonrpc
-    decorate with @services.jsonrpc the functions to expose
-    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
-    """
-    return service()
-
-
-@auth.requires_signature()
-def data():
-    """
-    http://..../[app]/default/data/tables
-    http://..../[app]/default/data/create/[table]
-    http://..../[app]/default/data/read/[table]/[id]
-    http://..../[app]/default/data/update/[table]/[id]
-    http://..../[app]/default/data/delete/[table]/[id]
-    http://..../[app]/default/data/select/[table]
-    http://..../[app]/default/data/search/[table]
-    but URLs must be signed, i.e. linked with
-      A('table',_href=URL('data/tables',user_signature=True))
-    or with the signed load operator
-      LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
-    """
-    return dict(form=crud())
+    binary = r.content
+    output = json.loads(binary)
+    if output["label"] == 'neg':
+        label = 'Negative'
+    elif output["label"] == 'pos':
+        label = 'Positive'
+    else:
+        label = 'Neutral'
+    print output["probability"]["neg"]
+    return dict(text=text,
+                label=label,
+                neg_prob=round(float(output["probability"]["neg"])*100),
+                neutral_prob=round(float(output["probability"]["neutral"])*100),
+                pos_prob=round(float(output["probability"]["pos"])*100))
